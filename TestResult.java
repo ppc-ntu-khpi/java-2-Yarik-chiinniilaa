810ef6669d32cd610735361948746b869cb04c77
Yarik-chiinniilaa <Tomatusik1@ukr.net>
2026-02-10 14:50:53 +0200

    Add files via upload

---
+++ TestResult.java
@@ -0,0 +1,6 @@
+public class TestResult {
+    public static void main(String[] args) {
+        System.out.println("Результат розрахунку:");
+        System.out.println(Exercise.Calculate("192.168.1.0/24"));
+    }
+}
